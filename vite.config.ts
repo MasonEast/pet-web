943c0532c90ed11d7669b0f2e4c5ef8b293c521e
liujianfeng <masoneastc@gmail.com>
2024-09-26 10:04:50 +0800

    添加less

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 添加以下选项以启用 JavaScript（这是可选的，根据需求）
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
