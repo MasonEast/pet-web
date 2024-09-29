999dd77b31132bb55fce78abcb3a050dada9d22b
liujianfeng <masoneastc@gmail.com>
2024-09-29 09:57:58 +0800

    首页开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
 import React from "react";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Breadcrumb, Layout, Menu, theme, Input } from "antd";
 
 import "./assets/global.less"; // 引入全局 less 样式
 import "./App.less";
 
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
-const items = new Array(15).fill(null).map((_, index) => ({
-  key: index + 1,
-  label: `nav ${index + 1}`,
-}));
+const items = [
+  {
+    key: 1,
+    label: "主页",
+  },
+  {
+    key: 2,
+    label: "城市",
+  },
+  {
+    key: 3,
+    label: "服务",
+  },
+  {
+    key: 4,
+    label: "主页",
+  },
+];
 
 const App: React.FC = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  const onSearch = (value: string) => console.log(value);
+
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <Menu
-          // theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={["2"]}
+          defaultSelectedKeys={["1"]}
           items={items}
-          style={{ flex: 1, minWidth: 0 }}
+          className="menu_left"
         />
+        <Search
+          placeholder="input search text"
+          allowClear
+          onSearch={onSearch}
+          className="search"
+        />
+        <Menu mode="horizontal" className="menu_right">
+          <Menu.Item key="5">登录</Menu.Item>
+          <Menu.Item key="6">注册</Menu.Item>
+        </Menu>
       </Header>
-      <Content style={{ padding: "0 48px" }}>
+      <Content style={{ padding: "0 48px", height: "3000px" }}>
         <Breadcrumb style={{ margin: "16px 0" }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
