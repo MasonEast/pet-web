5a7a778ff597428209376d914f5bc28e89731136
liujianfeng <masoneastc@gmail.com>
2024-10-14 14:34:17 +0800

    页面布局开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Layout, Menu, Input, Card } from "antd";
+import { Layout, Menu, Input, Card, Flex, Button, List, Avatar } from "antd";
+import banner from "@/assets/imgs/banner.jpeg";
+
+import ArticleList from "@/components/ArticleList";
 
 import "./assets/global.less"; // 引入全局 less 样式
 import "./App.less";
@@ -27,11 +30,22 @@ const items = [
   },
 ];
 
-const App: React.FC = () => {
-  // const {
-  //   token: { colorBgContainer },
-  // } = theme.useToken();
+const data = [
+  {
+    title: "Ant Design Title 1",
+  },
+  {
+    title: "Ant Design Title 2",
+  },
+  {
+    title: "Ant Design Title 3",
+  },
+  {
+    title: "Ant Design Title 4",
+  },
+];
 
+const App: React.FC = () => {
   const onSearch = (value: string) => console.log(value);
 
   return (
@@ -58,16 +72,57 @@ const App: React.FC = () => {
       <div className="content_wrapper">
         <Content className="content">
           <div className="left">
-            <Card hoverable cover={<img alt="example" src="/banner.jpeg" />}>
+            <Card hoverable cover={<img alt="example" src={banner} />}>
               <Meta
                 title="Europe Street beat"
                 description="www.instagram.com"
               />
             </Card>
-            <Card></Card>
+            <Card className="classify">
+              <Flex wrap gap="small">
+                {Array.from({ length: 24 }, (_, i) => (
+                  <Button key={i} type="primary">
+                    Button
+                  </Button>
+                ))}
+              </Flex>
+            </Card>
+            <Card>
+              <ArticleList />
+            </Card>
           </div>
           <div className="right">
-            <Card hoverable cover={<img alt="example" src="/banner.jpeg" />}>
+            <Card>
+              <Button>发布帖子</Button>
+              <Button>发布帖子</Button>
+            </Card>
+            <Card>
+              <div>金牌代办推荐</div>
+              <List
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={(item, index) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={
+                        <Avatar
+                          src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                        />
+                      }
+                      title={<a href="https://ant.design">{item.title}</a>}
+                      description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    />
+                  </List.Item>
+                )}
+              />
+            </Card>
+            <Card hoverable cover={<img alt="example" src={banner} />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+            <Card hoverable cover={<img alt="example" src={banner} />}>
               <Meta
                 title="Europe Street beat"
                 description="www.instagram.com"
